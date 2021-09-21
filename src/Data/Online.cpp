6883a8f3d6cf020e83666eee5e872b0f768074ca
zappatic <github@zappatic.net>
2021-09-21 13:19:05 +0200

    fix Online and ParticleDB

--- src/Data/Online.cpp
+++ src/Data/Online.cpp
@@ -16,9 +16,9 @@ bool S2Plugin::Online::loadOnline()
         return true;
     }
 
-    auto instructionOffset = Script::Pattern::FindMem(afterBundle, afterBundleSize, "F3 0F 11 4C 24 10 56 57");
-    auto relativeOffset = Script::Memory::ReadDword(instructionOffset + 27);
-    mOnlinePtr = Script::Memory::ReadQword(instructionOffset + 31 + relativeOffset);
+    auto instructionOffset = Script::Pattern::FindMem(afterBundle, afterBundleSize, "4C 8B 35 FA 61 35 00");
+    auto relativeOffset = Script::Memory::ReadDword(instructionOffset + 3);
+    mOnlinePtr = Script::Memory::ReadQword(instructionOffset + 7 + relativeOffset);
     refreshOffsets();
     return true;
 }
